3396feb4877eddec0d0f2dd7a5aa8522e7f7e9ab
oni <rainbow2774@naver.com>
2023-02-07 00:47:58 +0900

    스프링 배치 도메인 이해 - JobInstance

--- src/main/java/io/springbatch/springbatchlecture/JobInstanceConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobInstanceConfiguration.java
@@ -12,9 +12,9 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class JobConfiguration {
+public class JobInstanceConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -27,14 +27,12 @@ public class JobConfiguration {
                 .build();
     }
 
-
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("step1 was executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
@@ -43,15 +41,13 @@ public class JobConfiguration {
 
     @Bean
     public Step step2() {
-        return stepBuilderFactory.get("step2")
+        return stepBuilderFactory.get("step1")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("step2 was executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
                 .build();
     }
-
 }
